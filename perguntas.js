be174266d2ef2f4cefb93c804835b445682bf3fd
kawanmiguel <162150788+kawanmiguel@users.noreply.github.com>
2025-07-01 09:26:37 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
 
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
